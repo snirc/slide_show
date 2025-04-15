2c23f723b850e29f7cfa4b2165ebd9d3196c1884
snir cohen <snirco@gmail.com>
2025-04-16 02:12:47 +0300

    Initial commit for slide_show project

---
+++ src/main/java/com/snir/slide_show/SlideShowApplication.java
@@ -0,0 +1,13 @@
+package com.snir.slide_show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlideShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlideShowApplication.class, args);
+	}
+
+}
